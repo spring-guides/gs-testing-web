5e395f685a24b9c65afe2fca96b21a74bf6ffb16
Dave Syer <dsyer@pivotal.io>
2016-08-26 13:29:52 +0100

    Initial draft for testing

---
+++ complete/src/main/java/hello/GreetingController.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class GreetingController {
+
+    private final GreetingService service;
+
+    public GreetingController(GreetingService service) {
+        this.service = service;
+    }
+
+    @RequestMapping("/greeting")
+    public @ResponseBody String greeting() {
+        return service.greet();
+    }
+
+}
---
+++ complete/src/test/java/hello/WebMockTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package hello;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+//tag::test[]
+@RunWith(SpringRunner.class)
+@WebMvcTest(GreetingController.class)
+public class WebMockTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GreetingService service;
+
+    @Test
+    public void greetingShouldReturnMessageFromService() throws Exception {
+        when(service.greet()).thenReturn("Hello Mock");
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello Mock")));
+    }
+}
+// end::test[]
