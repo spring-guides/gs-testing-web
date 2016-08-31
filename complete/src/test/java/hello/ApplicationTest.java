09398e67da75754b863d204d05a5004d35b8cc9d
Dave Syer <david_syer@hotmail.com>
2016-08-31 07:55:01 +0100

    Remove license header

--- complete/src/test/java/hello/ApplicationTest.java
+++ complete/src/test/java/hello/ApplicationTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package hello;
 
 import static org.hamcrest.Matchers.containsString;
@@ -29,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-//tag::test[]
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -44,4 +28,3 @@ public class ApplicationTest {
                 .andExpect(content().string(containsString("Hello World")));
     }
 }
-// end::test[]
