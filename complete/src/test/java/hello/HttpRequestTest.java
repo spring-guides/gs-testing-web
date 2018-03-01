99cbb7dda72e7c237de990c48c7a20fe17b3fbab
Greg Turnquist <gturnquist@pivotal.io>
2018-03-01 09:55:14 -0600

    Upgrade to Spring Boot 2.0.0.RELEASE

--- complete/src/test/java/hello/HttpRequestTest.java
+++ complete/src/test/java/hello/HttpRequestTest.java
@@ -1,15 +1,16 @@
 package hello;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
