c876cea6f6c3d3d53b2ce8070d8502adc4967fc8
Jay Bryant <jbryant@pivotal.io>
2019-10-29 12:30:56 -0500

    Upgraded to Spring Boot 2.2
    I upgraded the build files to Spring Boot 2.2.0, which necessitated updating the tests to use JUnit 5 and replacing the Gradle wrappers.

--- complete/src/test/java/com/example/testingweb/SmokeTest.java
+++ complete/src/test/java/com/example/testingweb/SmokeTest.java
@@ -2,13 +2,11 @@ package com.example.testingweb;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmokeTest {
 
