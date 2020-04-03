aeebf4f19a8f789994ff164c62833a373c457c0d
Ali Akbar Moeini <61820390+AliAkbarMoeini@users.noreply.github.com>
2020-04-03 21:10:02 +0430

    Redundant SpringRunner import deletion

--- initial/src/test/java/com/example/testingweb/TestingWebApplicationTests.java
+++ initial/src/test/java/com/example/testingweb/TestingWebApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.testingweb;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 public class TestingWebApplicationTests {
