3282679de5cf81e59cace4ac1310184c7d656884
Jay Bryant <jbryant@vmware.com>
2022-12-19 12:38:38 -0600

    Upgraded to Spring Boot 3.0.0
    to get onto the new major version of Spring Boot.

--- complete/src/test/java/com/example/testingweb/HttpRequestTest.java
+++ complete/src/test/java/com/example/testingweb/HttpRequestTest.java
@@ -6,14 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.beans.factory.annotation.Value;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 
-	@LocalServerPort
+	@Value(value="${local.server.port}")
 	private int port;
 
 	@Autowired
