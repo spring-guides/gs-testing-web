fe8cdd5653124f8db0308da605a49cb9b91467f7
Jay Bryant <jbryant@vmware.com>
2023-10-20 09:20:35 -0500

    Back to using LocalServerPort.

--- complete/src/test/java/com/example/testingweb/HttpRequestTest.java
+++ complete/src/test/java/com/example/testingweb/HttpRequestTest.java
@@ -7,13 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HttpRequestTest {
 
-	@Value(value="${local.server.port}")
+	@LocalServerPort
 	private int port;
 
 	@Autowired
