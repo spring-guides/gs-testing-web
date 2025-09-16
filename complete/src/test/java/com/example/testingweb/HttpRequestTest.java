c7d975f94af3bb9c09ba4b5139468ca595c9b342
Danil.Pavlov <danil.pavlov@jetbrains.com>
2025-09-16 16:21:43 +0200

    Cleanup and upgrade to Spring Boot 3.5.5
    Signed-off-by: Danil Pavlov <danil.pavlov@jetbrains.com>

--- complete/src/test/java/com/example/testingweb/HttpRequestTest.java
+++ complete/src/test/java/com/example/testingweb/HttpRequestTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.assertj.core.api.Assertions.assertThat;
--- complete/src/test/java/com/example/testingweb/WebMockTest.java
+++ complete/src/test/java/com/example/testingweb/WebMockTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(GreetingController.class)
@@ -20,7 +20,7 @@ class WebMockTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@MockBean
+	@MockitoBean
 	private GreetingService service;
 
 	@Test
